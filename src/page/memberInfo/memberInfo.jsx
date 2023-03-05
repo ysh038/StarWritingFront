cebae79ebb1537c1745b3a13943ae382e41ef8ae
[username] <ysh038@naver.com>
2023-03-05 17:47:24 +0900

    이미지 로컬서버에 할당 후 클라이언트에서 불러옴

--- src/page/memberInfo/memberInfo.jsx
+++ src/page/memberInfo/memberInfo.jsx
@@ -1,15 +1,36 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import style from "./memberInfo.module.css";
 
 function MemberInfo() {
     const { id } = useParams();
+    const [memberInfo, setMemberInfo] = useState({});
+
+    const profileImg = document.getElementsByClassName(style.img);
 
     const getMemberInfo = () => {
         axios
             .get(`/api/members/${id}`)
             .then((res) => {
-                console.log(res.data);
+                setMemberInfo(res.data);
+                if (
+                    res.data.profileImage.originalFileName ===
+                    "basicProfile.png"
+                ) {
+                    profileImg[0].setAttribute(
+                        "src",
+                        "http://127.0.0.1:8887/basicProfile.png"
+                    );
+                } else {
+                    profileImg[0].setAttribute(
+                        "src",
+                        "http://127.0.0.1:8887/" +
+                            res.data.memberId +
+                            "/" +
+                            res.data.profileImage.storeFileName
+                    );
+                }
             })
             .catch((e) => {
                 console.error(e);
@@ -23,7 +44,13 @@ function MemberInfo() {
     return (
         <div>
             <h1>Here is MemberInfo Page.</h1>
-            <h2>id is {id}</h2>
+            <p>id is {memberInfo.id}</p>
+            <p>memberId is {memberInfo.memberId}</p>
+            <p>name is {memberInfo.name}</p>
+            <p>nickname is {memberInfo.nickname}</p>
+            <p>tier is {memberInfo.tier}</p>
+            <p>createDate is {memberInfo.createDate}</p>
+            <img className={style.img} alt="" />
         </div>
     );
 }
