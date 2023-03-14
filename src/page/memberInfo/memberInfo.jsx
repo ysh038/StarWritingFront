f4a3361f624cde016d53c4303950c4f7e9101185
[username] <ysh038@naver.com>
2023-03-14 13:00:06 +0900

    merge 완료

--- src/page/memberInfo/memberInfo.jsx
+++ src/page/memberInfo/memberInfo.jsx
@@ -8,60 +8,33 @@ function MemberInfo() {
     const [memberInfo, setMemberInfo] = useState({});
     const [imgData, setImgData] = useState();
 
-<<<<<<< HEAD
-    const profileImg = document.getElementsByClassName(style.profileImg);
-=======
-    // const profileImg = document.getElementsByClassName(style.img);
-    const pfImg = document.getElementsByClassName(style.pfImg);
->>>>>>> seungsu
-
     const getMemberInfo = () => {
         axios
             .get(`/api/members/${id}`)
             .then((res) => {
                 setMemberInfo(res.data);
-<<<<<<< HEAD
-                if (
-                    res.data.profileImage.originalFileName ===
-                    "basicProfile.png"
-                ) {
-                    profileImg[0].setAttribute(
-                        "src",
-                        "http://127.0.0.1:8887/img/profileImg/basicProfile.png"
-                    );
-                } else {
-                    profileImg[0].setAttribute(
-                        "src",
-                        "http://127.0.0.1:8887/members/" +
-                            res.data.memberId +
-                            "/profileImg/" +
-                            res.data.profileImage.storeFileName
-                    );
-                }
-=======
->>>>>>> seungsu
             })
             .catch((e) => {
                 console.error(e);
             });
-        
+
         const config = {
-            responseType: 'blob',
+            responseType: "blob",
             headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
+                "Content-type": "application/json; charset=UTF-8",
+            },
         };
-        
+
         axios
             .get(`/api/members/${id}/profile`, config)
             .then((res) => {
-                console.log(typeof (res.data));
+                console.log(typeof res.data);
                 console.dir(res.data);
                 setImgData(URL.createObjectURL(res.data));
             })
             .catch((e) => {
                 console.error(e);
-            })
+            });
     };
 
     useEffect(() => {
@@ -77,12 +50,8 @@ function MemberInfo() {
             <p>nickname is {memberInfo.nickname}</p>
             <p>tier is {memberInfo.tier}</p>
             <p>createDate is {memberInfo.createDate}</p>
-<<<<<<< HEAD
-            <img className={style.profileImg} alt="" />
-=======
             {/* <img className={style.img} alt="" /> */}
             <img className={style.pfImg} alt="" src={imgData} />
->>>>>>> seungsu
         </div>
     );
 }
